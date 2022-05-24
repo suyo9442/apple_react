475c9651b3ae6f98864a85078269d388fb214cd7
suyo9442 <72788139+suyo9442@users.noreply.github.com>
2022-05-24 18:36:36 +0900

    22/05/24 input다루기

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,6 +9,13 @@ function App() {
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
+
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+  
 
   return (
     <div className="App">
@@ -58,14 +65,11 @@ function App() {
         <p>2월 17일 발행</p>
       </div> */}
 
-      <button onClick={()=>{ setTitle(0) }}> 글제목0 </button>
-      <button onClick={()=>{ setTitle(1) }}> 글제목1 </button>
-      <button onClick={()=>{ setTitle(2) }}> 글제목2 </button>
 
       {
         글제목.map(function(a, i){
           return (
-            <div className="list" key={i}>
+            <div className="list" key={i} style={{overflow:'hidden'}}>
             <h4 onClick={ (e)=>{ 
                 if(modal == false) {
                   setModal(true) 
@@ -76,7 +80,8 @@ function App() {
                 setTitle(i)
               } }>
               { 글제목[i] }
-              <span onClick={ ()=>{
+              <span onClick={ (e)=>{
+                e.stopPropagation();
                 let 따봉copy = [...따봉];
                 따봉copy[i] += 1;
                 따봉변경(따봉copy) 
@@ -84,16 +89,51 @@ function App() {
               👍</span> 
               { 따봉[i] }
               </h4>
-            <p>2월 17일 발행</p>
+            <p>{`${year}년 ${month}월 ${date}일 발행`}</p>
+            <button style={{float: 'right'}} onClick={(e)=>{
+              let copy = [...글제목]
+              copy.splice(i, 1)
+              글제목변경(copy)
+            }}>삭제</button>
           </div>
           )
         })
       }
+
+
+      <input className='input' onChange={(e)=>{
+        입력값변경(e.target.value)
+        // console.log(입력값)
+      }} />
+      
+      <button onClick={()=>{
+        let input =  document.querySelector('.input')
+        let copy1 = [...글제목]
+        let copy2 = [...따봉]
+
+        if(input.value == '') {
+          return
+        } else {
+          // 입력값 추가
+          copy1.unshift(입력값)
+          글제목변경(copy1)
+          
+          // 따봉 추가
+          copy2.unshift(0)
+          따봉변경(copy2)
+
+          input.value = ''
+          input.focus()
+        }
+      }}>올리기</button>
+
+
       {
         // 조건식? 참일 때 실행할 코드 : 거짓일 때 실행할 코드
         // 1 == 1 ? '맞음' : '아님'
         modal == true ? <Modal color={'skyblue'} 글제목={글제목} 글제목변경={글제목변경} title={title}/> : null
       }
+
     </div>
   );
 } // App
@@ -102,7 +142,7 @@ function App() {
 function Modal(props) {
   return(
     <div className="modal" style={{background : props.color}}>
-        <h4>{ props.글제목[1] }</h4>
+        <h4>{ props.글제목[props.title] }</h4>
         <p>날짜</p>
         <p>상세내용</p>
         <button onClick={function(){
@@ -110,6 +150,15 @@ function Modal(props) {
           여자코트[0] = '여자 코트 추천'
           props.글제목변경(여자코트)          
         }}>글수정</button>
+    </div>
+  )
+}
+
+function List() {
+  return (
+    <div className="list">
+    <h4>{ 글제목[1] }</h4>
+    <p>2월 17일 발행</p>
     </div>
   )
 }
